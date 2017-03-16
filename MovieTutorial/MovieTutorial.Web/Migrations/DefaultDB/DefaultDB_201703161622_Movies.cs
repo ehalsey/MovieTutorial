9be31c651a9244de216471050beb29219fdcd1e2
ehalsey <ehalsey@a-cto.com>
2017-03-16 16:29:30 -0700

    before change db

---
+++ MovieTutorial/MovieTutorial.Web/Migrations/DefaultDB/DefaultDB_201703161622_Movies.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+using System;
+
+namespace MovieTutorial.Migrations.DefaultDB
+{
+    [Migration(201703161622)]
+    public class DefaultDB_201703161622_Movies : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Create.Schema("mov");
+            Create.Table("Movie").InSchema("mov")
+                .WithColumn("MovieId").AsInt32()
+                    .Identity().PrimaryKey().NotNullable()
+                .WithColumn("Title").AsString(200).NotNullable()
+                .WithColumn("Description").AsString(1000).Nullable()
+                .WithColumn("Storyline").AsString(Int32.MaxValue).Nullable()
+                .WithColumn("Year").AsInt32().Nullable()
+                .WithColumn("ReleaseDate").AsDateTime().Nullable()
+                .WithColumn("Runtime").AsInt32().Nullable();
+        }
+    }
+}
